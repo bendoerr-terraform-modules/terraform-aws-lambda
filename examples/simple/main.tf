e4fc22be07f92e7b396cd6f65fa656db586d7885
Ben Doerr <craftsman@bendoerr.me>
2025-04-20 19:15:48 -0400

    🎉 new: Adds the functionality for a basic lambda module

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -8,6 +8,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.7"
+    }
   }
 }
 
@@ -15,8 +20,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "archive_file" "hello" {
+  output_path = "${path.module}/lambda-src/hello.zip"
+  type        = "zip"
+  source_file = "${path.module}/lambda-src/hello.js"
+}
+
 module "this" {
   source  = "../.."
   context = module.context.shared
-  name    = "example"
+  name    = "simple"
+
+  description = "A simple example"
+  filename    = data.archive_file.hello.output_path
+  handler     = "hello.handler"
+  runtime     = "nodejs22.x"
 }
